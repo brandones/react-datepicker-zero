e0f2fc092e6293f386c7a0af4674eb7fecb38eb6
Brandon Istenes <bistenes@gmail.com>
2020-04-26 22:04:30 -0700

    jest, prettier, eslint, husky, travis

--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,3 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Datepicker from "./Datepicker";
 ReactDOM.render(<Datepicker />, document.getElementById("root"));
-
--- webpack.config.js
+++ webpack.config.js
@@ -10,21 +10,21 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
         loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
-      }
-    ]
+        options: { presets: ["@babel/env"] },
+      },
+    ],
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
     path: path.resolve(__dirname, "dist/"),
     publicPath: "/dist/",
-    filename: "bundle.js"
+    filename: "bundle.js",
   },
   devServer: {
     contentBase: path.join(__dirname, "public/"),
     port: 3000,
     publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
+    hotOnly: true,
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  plugins: [new webpack.HotModuleReplacementPlugin()],
 };
